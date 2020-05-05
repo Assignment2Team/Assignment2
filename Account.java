2207bfcc18fe6a373c705a63cdbbb72b93604a25
UzairLakhani <lakhaniuzair780@gmail.com>
2020-05-05 18:25:45 -0400

    Update and rename Atm to Account.java

---
+++ Account.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Accounts {
+    private String username;
+    private String password;
+    private int money;
+    private int deposit;
+    private int withdraw;
+    
+    public Accounts (String username, String password, int money){
+        this.username = username;
+        this.password = password;
+        this.money = money;
+    }
+    
+    public String getUserName(){
+        return username;
+     
+    }
+    
+    public String getPassWord(){
+        return password;
+    }
+    
+    public int getMoney(){
+        return money;
+    }
+    
+    public void setMoney(int newMoney){
+        money = newMoney;
+    }
+        
+    @Override
+    public String toString(){
+        return "-------------------------------------" 
+                 
+                + "\nPlease select one of the options below "	 
+		 + "\nPress (1) to  deposit"
+		 + "\nPress (2) to transfer money"
+          	+"\nPress (3) to withdraw"  
+		+ "\nPress (3) to  print balance" 
+		 +"\nPress (3) to switch accounts"
+		 +"\nPress (6) to exit";
+                 
+    }
+    
+}

16851785bd56e66add68fa985367709154a0e5aa
UzairLakhani <lakhaniuzair780@gmail.com>
2020-05-10 19:50:16 -0400

    Update Mainatm.java

--- Mainatm.java
+++ Mainatm.java
@@ -1,5 +1,17 @@
-import java.util.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Atm;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Uzair, Dharmin and Arsjit
+ */
 public class Atm {
 public static void main(String[] args) {
 	       

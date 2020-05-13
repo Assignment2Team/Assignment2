e32f5b4d6081c7c3e42dae296353ef7f0ad479b2
UzairLakhani <lakhaniuzair780@gmail.com>
2020-05-13 16:58:32 -0700

    Update Reciepts.java

--- Reciepts.java
+++ Reciepts.java
@@ -7,51 +7,55 @@ package Atm;
 
 /**
  *
- * @author Nanu
+ * @author Arshjit Sansoe
  */
-public class Reciepts {
-    
-    private int[] bills = {100,50,20,10,5};
-    private int hundred=0;int fifty=0;int twenty=0;int ten=0;int five=0;
-    private String recipts;	
-    
-    public Reciepts (String recipts){
-        
-        this.recipts = recipts;
-	
-    }
-      		
-	
-    public String getRecepts(){
-        return recipts;
-    }
-    
-     public String toString() {
-        return "*******************************************"
-                + "\n ***********>>>Vanier BANK<<<***********"
-                + "\n *----------Transaction  Receipt**********"
-                + "\n*******************************************"
-                + "\n*******************************************";
-    }
-     
-      public String withdraw() {
-          
-           return  "************************************"+
-                "\n*Hundred:           "+ hundred     + 
-                "\n*Fifties:           " + fifty      +
-                "\n*Twenties:  "        + twenty  +
-                "\n*Tens:        "      +ten  +
-                 "\n*Fives:        "    +five  +   
-                "\n**********************************";      
-    }
-    
-    //public boolean equals(Reciepts book2){
-    //if (password.equals(book2.publisher) && bookAmount == book2.bookAmount){
-    //return true;
-    //} 
-    //else {
-    //return false;
-    }
-                    
-                     
+class Receipts {
+  private int hundred=0;int fifty=0;int twenty=0;int ten=0;int five=0;
 
+  public Receipts (int hundred, int fifty, int twenty, int ten, int five){
+    this.hundred=hundred;
+    this.fifty=fifty;
+    this.twenty=twenty;
+    this.ten=ten;
+    this.five=five;
+  }
+  public void setHundreds(int receipthundred){
+    hundred=receipthundred;  
+  }
+  public void setFifty(int receiptFifty){
+    fifty=receiptFifty;  
+  }
+  public void setTwenty(int receiptTwenty){
+    twenty=receiptTwenty;  
+  }
+  public void setTen(int receiptTen){
+    ten=receiptTen;  
+  }
+  public void setFive(int receiptFive){
+    five=receiptFive;  
+  }
+  public int getHundred(){
+      return hundred;
+  }
+  public int getFifty(){
+      return fifty;
+  }
+  public int getTwenty(){
+      return twenty;
+  }
+  public int getTen(){
+      return ten;
+  }
+  public int getFive(){
+      return five;
+  }
+  public String withdraw() {
+    return  "************************************"+
+    "\n*Hundred(s):           "+ hundred     +
+    "\n*Fiftie(s):            "+ fifty       +
+    "\n*Twentie(s):           "+ twenty      +
+    "\n*Ten(s):               "+ ten         +
+    "\n*Five(s):              "+ five        +
+    "\n**********************************";
+  }
+}

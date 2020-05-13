b85d390c70312094a2971b8129d40c67c0767227
UzairLakhani <lakhaniuzair780@gmail.com>
2020-05-13 15:13:26 -0700

    Javadoc and inline comments

--- Account.java
+++ Account.java
@@ -1,44 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Atm;
 
 /**
- *
- * @author Uzair
+ * This class creates accounts the Main class.
+ * It also contains constructors and methods for the Main class.
+ * 
+ * @author Uzair Lakhani
  */
-
-class Account {
-	
-    private String password;	
-    private double total;
-    private double deposit;
+class Accountfinal {
     
-    public Account(String password,double money){
-        
-        this.password = password;
-	    this.total = money;
-    }
-    public double getMoney(){
-        return total;
-    }
-    public void setMoney(double newMoney){
-      total= newMoney;
-    }
-    public String getPassWord(){
-        return password;
-    }
-      public String toString(){
-        return "*********************************************" 
-                 
-         + "\nPlease select one of the options below "	 
-		 + "\nPress (1) to  Switch Accounts"
-		 + "\nPress (2) to Deposit (2 decimal places)"
-        	 +"\nPress (3) Transfer (2 decimal places)"  
-		 + "\nPress (4) to  Withdraw (2 decimal places)" 
-		 +"\nPress (5) to Print balance"
-		 +"\nPress (6) to Exit";
-      }
+  //Creates attributes of an atm machine  
+  private int pinNumber;
+  private double total;
+  private double deposit;
+  private String userName;
+  
+    /**
+     * Constructor with parameters.
+     *
+     * @param username
+     * @param pinNumber
+     * @param money
+     */
+  public Accountfinal(String userName, int pinNumber, double money){
+    this.pinNumber = pinNumber;
+    this.total = money;
+    this.userName = userName;
+  }
+
+    /**
+     * Returns the username.
+     *
+     * @return
+     */
+  
+  public String getUserName(){
+    return userName;
+  }
+    /**
+     * Returns total amount.
+     *
+     * @return
+     */
+  public double getMoney(){
+    return total;
+  }
+  
+     /**
+     * Returns the pinNumber.
+     *
+     * @return
+     */
+  
+  public int getPinNumber(){
+    return pinNumber;
+  }
+  
+    /**
+     * Method to set total amount of money.
+     *
+     * @param newMoney
+     */
+  
+  public void setMoney(double newMoney){
+    total= newMoney;
+  }
+
+
+    /**
+    *toString method of account class to print options in the Main class
+    * @return
+     */
+    @Override  
+  public String toString(){
+    return "*********************************************"
+    + "\nPlease select one of the options below: "
+    + "\nPress (1) to  Switch Accounts"
+    + "\nPress (2) to Deposit (2 decimal places)"
+    +"\nPress (3) Transfer (2 decimal places)"
+    + "\nPress (4) to  Withdraw (2 decimal places)"
+    +"\nPress (5) to Print balance"
+    +"\nPress (6) to Exit";
+  }
 }

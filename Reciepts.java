4023b9ab7b4865d7f4e4a56547567e84768d1044
ARSH5588 <60395168+ARSH5588@users.noreply.github.com>
2020-05-13 23:03:48 -0400

    Update Reciepts.java

--- Reciepts.java
+++ Reciepts.java
@@ -1,54 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Atm;
-
 /**
- *
+ * This class will make receipts for the withdraw method.
  * @author Arshjit Sansoe
  */
 class Receipts {
-  private int hundred=0;int fifty=0;int twenty=0;int ten=0;int five=0;
+    private static int hundred=0;int fifty=0;int twenty=0;int ten=0;int five=0;
 
-  public Receipts (int hundred, int fifty, int twenty, int ten, int five){
-    this.hundred=hundred;
-    this.fifty=fifty;
-    this.twenty=twenty;
-    this.ten=ten;
-    this.five=five;
-  }
-  public void setHundreds(int receipthundred){
-    hundred=receipthundred;  
-  }
-  public void setFifty(int receiptFifty){
-    fifty=receiptFifty;  
-  }
-  public void setTwenty(int receiptTwenty){
-    twenty=receiptTwenty;  
-  }
-  public void setTen(int receiptTen){
-    ten=receiptTen;  
-  }
-  public void setFive(int receiptFive){
-    five=receiptFive;  
-  }
-  public int getHundred(){
-      return hundred;
-  }
-  public int getFifty(){
-      return fifty;
-  }
-  public int getTwenty(){
-      return twenty;
-  }
-  public int getTen(){
-      return ten;
-  }
-  public int getFive(){
-      return five;
-  }
+    /**
+     * Constructor
+     * @param hundred - int hundred
+     * @param fifty - int fifty
+     * @param twenty - int twenty
+     * @param ten - int ten
+     * @param five - int five
+     * @return amount of withdrawn bills
+     */
+    public Receipts (int hundred, int fifty, int twenty, int ten, int five){
+        this.hundred=hundred;
+        this.fifty=fifty;
+        this.twenty=twenty;
+        this.ten=ten;
+        this.five=five;
+    }
+
+    /**
+     * @param receiptHundred - sets hundred bills value
+     * @return null
+     */
+    //setters set all the amount(s) of bills
+    public void setHundreds(int receiptHundred){
+        hundred=receiptHundred;
+    }
+
+    /**
+     * @param receiptFifty - sets fifty bills value
+     * @return null
+     */
+    public void setFifty(int receiptFifty){
+        fifty=receiptFifty;
+    }
+
+    /**
+     * @param receiptTwenty - sets twenty bills value
+     */
+    public void setTwenty(int receiptTwenty){
+        twenty=receiptTwenty;
+    }
+
+    /**
+     * @param receiptTen - sets ten bills value
+     * @return null
+     */
+    public void setTen(int receiptTen){
+        ten=receiptTen;
+    }
+
+    /**
+     * @param receiptFive - sets five bills value
+     * @return null
+     */
+    public void setFive(int receiptFive){
+        five=receiptFive;
+    }
+
+    /**
+     * @return hundred - gives a 100 dollar bill
+     */
+    //getters output the amount(s) of bills
+    public int getHundred(){
+        return hundred;
+    }
+
+    /**
+     * @return fifty - gives a 50 dollar bill
+     */
+    public int getFifty(){
+        return fifty;
+    }
+
+    /**
+     * @return - gives a 20 dollar bill
+     */
+    public int getTwenty(){
+        return twenty;
+    }
+
+    /**
+     * @return - gives a 10 dollar bill
+     */
+    public int getTen(){
+        return ten;
+    }
+
+    /**
+     * @return five - gives a 5 dollar bill
+     */
+    public int getFive(){
+        return five;
+    }
+
+    /**
+     * This prints out the withdrawn amount of bills
+     */
+    //prints the amount of bills for withdraw
   public String withdraw() {
     return  "************************************"+
     "\n*Hundred(s):           "+ hundred     +
